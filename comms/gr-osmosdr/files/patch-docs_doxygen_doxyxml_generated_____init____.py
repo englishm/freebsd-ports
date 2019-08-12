6b5a3c2d216c72f03a2ddf5339c6972c8c2e9a49
Mike English <mike.english@gmail.com>
2019-08-12 12:30:51 -0400

    comms/gr-osmosdr: Consolidate gr3.8 patches

--- comms/gr-osmosdr/files/patch-docs_doxygen_doxyxml_generated_____init____.py
+++
@@ -1,7 +0,0 @@
---- docs/doxygen/doxyxml/generated/__init__.py.orig	2018-08-15 17:53:26 UTC
-+++ docs/doxygen/doxyxml/generated/__init__.py
-@@ -5,3 +5,4 @@ These do the real work of parsing the doxygen xml file
- resultant classes are not very friendly to navigate so the rest of the
- doxyxml module processes them further.
- """
-+from __future__ import unicode_literals
